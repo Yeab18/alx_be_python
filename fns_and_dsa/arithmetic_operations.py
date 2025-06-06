f783b81397eabb79e8e939bff9319cbd3ee8e970
Yeabsira Taye <yeabsira.taye@aastustudent.edu.et>
2025-06-06 15:13:18 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 # arithmetic_operations.py
 
-def perform_operation (num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -10,6 +10,7 @@ def perform_operation (num1: float, num2: float, operation: str):
     elif operation == 'divide':
         if num2 == 0:
             return "Error: Division by zero"
-        return num1 / num2
+        else:
+            return num1 / num2
     else:
         return "Error: Invalid operation"
